bf2f0fc38a64ba449d6e7638714dc213e5c65b60
Crowdbotics <team@crowdbotics.com>
2023-07-16 11:40:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dispatch-locate-42213",
+  url: "https://dispatch-locate-42213.botics.co",
+  api: "https://dispatch-locate-42213.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
